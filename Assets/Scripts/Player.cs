5582cbb4922c89aa991ca3e52c7e6336e0e4faa3
Alkan <atakan.alkan@sabanciuniv.local>
2024-01-14 12:45:04 +0300

    player health bar & dig mechanic scene added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,18 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player1 : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Collider2D collider;
     private AudioSource jumpSound;
-    
     private int jumpCount;
+    
+    public int maxHealth = 100;
+
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         collider = GetComponent<Collider2D>();
         jumpSound = GetComponent<AudioSource>();
+
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     private void Awake()
@@ -29,10 +39,17 @@ public class Player : MonoBehaviour
             jumpCount = 2;
         }
     }
-
-    // Update is called once per frame
     void Update()
+    
     {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            TakeDamage(20);
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            GainHealth(20);
+        }
         if (Input.GetButtonDown("Jump"))
         {
             if (jumpCount > 0)
@@ -65,6 +82,19 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.S))
         {
             //rb.AddForce(new Vector2(-1,0));
+        }
+        
+        void TakeDamage(int damage)
+        {
+            currentHealth -= damage;
+
+            healthBar.SetHealth(currentHealth);
+        }
+        void GainHealth(int health)
+        {
+            currentHealth += health;
+
+            healthBar.SetHealth(currentHealth);
         }
     }
 }
