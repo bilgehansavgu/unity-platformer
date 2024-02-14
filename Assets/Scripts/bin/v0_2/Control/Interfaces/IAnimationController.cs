6facbaf8d87d222974b195f9c9a75174b49c5518
Alkan <atakan.alkan@sabanciuniv.local>
2024-02-15 01:53:28 +0300

    bin removed

--- Assets/Scripts/bin/v0_2/Control/Interfaces/IAnimationController.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IAnimationController
-{
-    void PlayAnimation(string animationName);
-    float GetAnimationLength(string animationName);
-}
--- Assets/Scripts/bin/v0_2/Control/Interfaces/IComboSystem.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IComboSystem
-{
-    event System.Action OnAnimationFinished;
-}
