670aa47d6f2cc5b4cb31b5224b48d1533c85f3b4
sefaKscu <sefakscu@gmail.com>
2024-02-17 03:18:25 +0300

    StateLogic Implementation

--- Assets/Scripts/Core/PlayerController/PlayerController.cs
+++ Assets/Scripts/Core/PlayerController/PlayerController.cs
@@ -19,7 +19,9 @@ namespace Core.CharacterController
         {
             Idle,
             Move,
+            AnticipateJump,
             Jump,
+            Falling,
             Landing,
             SquareAttack
         }
@@ -31,6 +33,8 @@ namespace Core.CharacterController
                 { StateID.Idle, new PlayerState_Idle(this) },
                 { StateID.Move, new PlayerState_Move(this) },
                 { StateID.Jump, new PlayerState_Jump(this) },
+                { StateID.Falling, new PlayerState_Falling(this) },
+                { StateID.Landing, new PlayerState_Landing(this) },
                 { StateID.SquareAttack, new PlayerState_SquareAttack(this) }
             };
             fsm = new StateMachine<StateID>(states, new PlayerState_Any(fsm), StateID.Idle);
@@ -54,8 +58,23 @@ namespace Core.CharacterController
         {
             GetInputs();
             fsm.Tick();
+            CountAttackCooldown();
         }
-
+        float attackCooldown = 0;
+        public bool ReadyToAttack => attackCooldown <= 0;
+        private void CountAttackCooldown()
+        {
+            if (attackCooldown > 0)
+                attackCooldown -= Time.deltaTime;
+            else
+            {
+                attackCooldown = 0;
+            }
+        }
+        public void SetAttackCooldown(float cooldown)
+        {
+            attackCooldown = cooldown;
+        }
         private void GetInputs()
         {
             if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
@@ -70,10 +89,6 @@ namespace Core.CharacterController
         {
             RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundLayer);
             return hit.collider != null && !hit.collider.isTrigger;
-        }
-        public void OnAnimationFinished(StateID stateToTrigger)
-        {
-            fsm.GetState(stateToTrigger).Exit(fsm);
         }
     }
     public class PlayerState_Idle : PlayerState_Base
@@ -100,8 +115,10 @@ namespace Core.CharacterController
                 machine.ChangeState(PlayerController.StateID.Move);
             if (parent.Inputs.jumpTriggered)
                 machine.ChangeState(PlayerController.StateID.Jump);
-            if (parent.Inputs.attackSquareActionTriggered)
+            if (parent.Inputs.attackSquareActionTriggered && parent.ReadyToAttack)
                 machine.ChangeState(PlayerController.StateID.SquareAttack);
+            if (!parent.IsGrounded() && !parent.Inputs.jumpTriggered)
+                machine.ChangeState(PlayerController.StateID.Falling);
         }
     }
     public class PlayerState_Move : PlayerState_Base
@@ -137,8 +154,10 @@ namespace Core.CharacterController
                 machine.ChangeState(PlayerController.StateID.Idle);
             if (parent.Inputs.jumpTriggered)
                 machine.ChangeState(PlayerController.StateID.Jump);
-            if (parent.Inputs.attackSquareActionTriggered)
+            if (parent.Inputs.attackSquareActionTriggered && parent.ReadyToAttack)
                 machine.ChangeState(PlayerController.StateID.SquareAttack);
+            if (!parent.IsGrounded() && !parent.Inputs.jumpTriggered)
+                machine.ChangeState(PlayerController.StateID.Falling);
         }
     }
     public class PlayerState_Jump : PlayerState_Base
@@ -206,6 +225,65 @@ namespace Core.CharacterController
         {
             if (parent.IsGrounded())
                 machine.ChangeState(PlayerController.StateID.Landing);
+            if (parent.Inputs.attackSquareActionTriggered && parent.ReadyToAttack)
+                machine.ChangeState(PlayerController.StateID.SquareAttack);
+            // Switch to fall state when maxHeight reached
+        }
+    }
+    public class PlayerState_Falling : PlayerState_Base
+    {
+        const string fallClip = "jump_animation";
+        private float _maxMovementVelocity = 5f;
+        private float jumpLoad = 3f;
+        private float fallLoad = 4f;
+        public PlayerState_Falling(PlayerController parent) : base(parent)
+        {
+        }
+
+        public override PlayerController.StateID GetID() => PlayerController.StateID.Falling;
+
+        public override void Enter(StateMachine<PlayerController.StateID> machine)
+        {
+            PlayClip(fallClip);
+        }
+
+        public override void Exit(StateMachine<PlayerController.StateID> machine)
+        {
+        }
+
+
+        protected override void Act(StateMachine<PlayerController.StateID> machine)
+        {
+            if (parent.Rb2D.velocity.y <= 0)
+            {
+                parent.Rb2D.velocity += Vector2.up * (Physics2D.gravity.y * fallLoad * Time.deltaTime);
+            }
+            if (parent.Inputs.MoveInputValue.x > 0)
+            {
+                parent.transform.rotation = Quaternion.Euler(0, 0, 0);
+                if (parent.Rb2D.velocity.x < _maxMovementVelocity)
+                {
+                    float speedDifference = Mathf.Abs(_maxMovementVelocity - parent.Rb2D.velocity.x);
+                    parent.Rb2D.velocity += new Vector2(speedDifference, 0);
+                }
+            }
+            else if (parent.Inputs.MoveInputValue.x < 0)
+            {
+                parent.transform.rotation = Quaternion.Euler(0, 180, 0);
+                if (parent.Rb2D.velocity.x > -_maxMovementVelocity)
+                {
+                    float speedDifference = Mathf.Abs(_maxMovementVelocity + parent.Rb2D.velocity.x);
+                    parent.Rb2D.velocity += new Vector2(-speedDifference, 0);
+                }
+            }
+        }
+
+        protected override void Decide(StateMachine<PlayerController.StateID> machine)
+        {
+            if (parent.IsGrounded())
+                machine.ChangeState(PlayerController.StateID.Landing);
+            if (parent.Inputs.attackSquareActionTriggered && parent.ReadyToAttack)
+                machine.ChangeState(PlayerController.StateID.SquareAttack);
         }
     }
     public class PlayerState_Landing : PlayerState_Base
@@ -216,14 +294,15 @@ namespace Core.CharacterController
         }
         public override PlayerController.StateID GetID() => PlayerController.StateID.Landing;
 
-        public override void Enter(StateMachine<PlayerController.StateID> machine) => PlayClip(landingClip);
+        public override void Enter(StateMachine<PlayerController.StateID> machine)
+        {
+            PlayClip(landingClip);
+            counter = 0.2f;
+        }
 
         public override void Exit(StateMachine<PlayerController.StateID> machine)
         {
-            if (parent.IsMoving)
-                machine.ChangeStateImmediate(PlayerController.StateID.Move);
-            else
-                machine.ChangeStateImmediate(PlayerController.StateID.Idle);
+
         }
 
 
@@ -233,6 +312,15 @@ namespace Core.CharacterController
         float counter;
         protected override void Decide(StateMachine<PlayerController.StateID> machine)
         {
+            if (counter > 0)
+                counter -= Time.deltaTime;
+            else
+            {
+                if (parent.IsMoving)
+                    machine.ChangeStateImmediate(PlayerController.StateID.Move);
+                else
+                    machine.ChangeStateImmediate(PlayerController.StateID.Idle);
+            }
         }
     }
     public class PlayerState_SquareAttack : PlayerState_Base
@@ -243,10 +331,17 @@ namespace Core.CharacterController
         }
         public override PlayerController.StateID GetID() => PlayerController.StateID.SquareAttack;
 
-        public override void Enter(StateMachine<PlayerController.StateID> machine) => PlayClip(attack);
+        public override void Enter(StateMachine<PlayerController.StateID> machine)
+        {
+            PlayClip(attack);
+            counter = 1f;
+            parent.Rb2D.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
 
         public override void Exit(StateMachine<PlayerController.StateID> machine)
         {
+            parent.Rb2D.constraints = RigidbodyConstraints2D.FreezeRotation;
+            parent.SetAttackCooldown(0.7f);
         }
 
 
@@ -255,8 +350,18 @@ namespace Core.CharacterController
         {
         }
 
+        float counter;
         protected override void Decide(StateMachine<PlayerController.StateID> machine)
         {
+            if (counter > 0)
+                counter -= Time.deltaTime;
+            else
+            {
+                if (parent.IsGrounded())
+                    machine.ChangeState(PlayerController.StateID.Idle);
+                else
+                    machine.ChangeState(PlayerController.StateID.Falling);
+            }
         }
     }
 }
